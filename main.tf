28fa027e616706db805561cea99d3b5052d58317
hou-zp <65334730+hou-zp@users.noreply.github.com>
2021-10-08 14:11:57 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "alicloud" {}
 
 resource "alicloud_vpc" "vpc" {
   vpc_name   = "tf_test_foo"
-  cidr_block = "172.16.0.0/12"
+  cidr_block = "172.16.0.0/16"
 }
 
 resource "alicloud_vswitch" "vsw" {
   vpc_id            = alicloud_vpc.vpc.id
-  cidr_block        = "172.16.0.0/21"
+  cidr_block        = "172.16.0.0/24"
   zone_id           = "cn-beijing-b"
 }
 
