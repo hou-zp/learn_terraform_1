ba10ddc461305c1a018a81509c737ab4447c4183
hou-zp <65334730+hou-zp@users.noreply.github.com>
2021-10-08 14:34:18 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,41 @@
 provider "alicloud" {
 }
 
-resource "alicloud_security_group" "group" {
-  name        = "tf_test_foo"
-  description = "foo"
-  vpc_id      = alicloud_vpc.vpc.id
-}
-
 resource "alicloud_vpc" "vpc" {
-  name       = "test_vpc"
+  vpc_name   = "tf_test_vpc"
   cidr_block = "172.16.0.0/16"
 }
 
-resource "alicloud_vswitch" "vswitch" {
+resource "alicloud_vswitch" "vsw" {
   vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "172.16.0.0/24"
-  vswitch_name      = "test_vpc"
-  availability_zone = "cn-beijing-b"
+  zone_id           = "cn-beijing-b"
+}
+
+resource "alicloud_security_group" "asg" {
+  name = "tf_test_asg"
+  vpc_id = alicloud_vpc.vpc.id
 }
 
 resource "alicloud_instance" "instance" {
   # cn-beijing
-  availability_zone = "cn-beijing-b"
-  security_groups   = alicloud_security_group.group.*.id
-
+  availability_zone = "{$var.az}"
+  security_groups = alicloud_security_group.default.*.id
   # series III
-  instance_type              = "ecs.n2.small"
-  image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
-  instance_name              = "test_foo"  
-  vswitch_id                 = alicloud_vswitch.vswitch.id
+  instance_type        = "ecs.n2.small"
+  image_id             = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name        = "test_foo"
+  vswitch_id = alicloud_vswitch.vsw.id
   internet_max_bandwidth_out = 10
+}
+
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
+  cidr_ip           = "0.0.0.0/0"
 }
