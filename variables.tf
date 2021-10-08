bfd77f7010a70bfb334cbfa172c46f3eaf8e0dfb
hou-zp <1531737171@qq.com>
2021-10-08 10:07:55 +0800

    testterraform

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "alicloud region"
+  default     = "cn-hangzhou"
+  type        = string
+}
+
+variable "az" {
+  default     = "cn-beijing-b"
+  description = "avaibility zone"
+}
